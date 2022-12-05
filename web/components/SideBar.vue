98f335cc6949c5b941c10bfd64c90e36d81b5c32
root <root@dev-box.localdomain>
2022-12-05 02:16:09 +0000

    Starting web gui

---
+++ web/components/SideBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen w-50 border-r border-zinc-800">
+    sidebar
+  </div>
+</template>
---
+++ web/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="h-screen w-full bg-zinc-900 text-zinc-300 flex">
+    <SideBar/>
+    <slot/>
+  </div>
+</template>
